812b7bfb83b97c9c76e378de4baffc8cbcaa380a
harshhhhss <harshsinghhhh26@gmail.com>
2026-04-27 17:39:32 +0530

    problem 1 - two sum

---
+++ Arrays/001_Two_Sum.cpp
@@ -0,0 +1,21 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) { 
+        for (int i = 0; i < nums.size(); i++){
+            for (int j = i + 1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
