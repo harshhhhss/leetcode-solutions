35395e59f59041b2d01c8418b30beff066214217
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-01 15:44:40 +0530

    problem 202 - happy number

---
+++ Math/202_Happy_Number.cpp
@@ -0,0 +1,23 @@
+// Problem: Happy Number
+// Difficulty: Easy
+// Topic: Math, Recursion
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n == 1 || n == 7) {
+            return true;
+        }
+        if (n < 10) {
+            return false;
+        }
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return isHappy(sum);
+    }
+};
