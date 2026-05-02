b34c676086511c78b2cdf0bfd3c399c3a1e75856
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-02 16:12:48 +0530

    problem 66 plus one

---
+++ Arrays/066_Plus_One.cpp
@@ -0,0 +1,21 @@
+// Problem: Remove Element 
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if (digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
