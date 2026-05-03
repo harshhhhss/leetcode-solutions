48fc2613fb2bf63e03f087cd9ba3bd86f81b6dd7
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-03 16:30:26 +0530

    problem 69 Sqrt(x)

---
+++ Arrays/069_Sqrt.cpp
@@ -0,0 +1,14 @@
+// Problem: Sqrt(x)
+// Difficulty: Easy
+// Topic: Arrays,Math
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int y=pow(x,0.5);
+        return y;
+    }
+};
