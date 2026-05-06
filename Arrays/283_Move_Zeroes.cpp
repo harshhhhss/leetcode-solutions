0233c0371287410b966e750ae0b934ade1366229
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-06 17:08:33 +0530

    problem 283 move zeroes

---
+++ Arrays/283_Move_Zeroes.cpp
@@ -0,0 +1,19 @@
+// Problem: Move Zeroes
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
