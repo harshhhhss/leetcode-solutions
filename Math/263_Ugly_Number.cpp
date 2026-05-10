57f944d9b59b44fa176c7b5313aa071d25a6ed4b
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-10 11:37:49 +0530

    problem 263 ugly number

---
+++ Math/263_Ugly_Number.cpp
@@ -0,0 +1,25 @@
+// Problem: Ugly Number
+// Difficulty: Easy
+// Topic: Math
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n<=0){
+            return false;
+        } while(n>1){
+            if(n%2==0){
+                n=n/2;
+            } else if(n%3==0){
+                n=n/3;
+            } else if(n%5==0){
+                n=n/5;
+            }
+            else break;
+        }
+        return (n==1);
+    }
+};
