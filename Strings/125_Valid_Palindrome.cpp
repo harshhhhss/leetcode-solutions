591b44d072bca20a3531cff7b515d316739d1a76
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-12 08:45:04 +0530

    problem 242 valid palindrome

---
+++ Strings/125_Valid_Palindrome.cpp
@@ -0,0 +1,28 @@
+// Problem: Valid Anagram
+// Difficulty: Easy
+// Topic: Strings 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(!isalnum(s[i])){
+                i++;
+            } else if(!isalnum(s[j])){
+                j--;
+            } else{
+                if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
