163814ac19a96fc01fa9ddd20daed2af28490fc2
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-17 16:56:32 +0530

    problem 941 valid mountain array

---
+++ Arrays/941_Valid_Mountain_Array.cpp
@@ -0,0 +1,27 @@
+// Problem: Valid Mountain Array
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        if(n<3){
+            return false;
+        }
+        int i=0;
+        while(i<n-1 && arr[i]<arr[i+1]){
+            i++;
+        }
+        if(i==0 || i==n-1){
+            return false;
+        }
+        while(i<n-1 && arr[i]>arr[i+1]){
+            i++;        
+        }
+        return i==n-1;
+    }
+};
