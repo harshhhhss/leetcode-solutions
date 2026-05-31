9df4f33f7ed8df736f70862c1bcb079501a84ed0
harshhhhss <harshsinghhhh26@gmail.com>
2026-05-31 17:55:19 +0530

    problem 278 first bad version

---
+++ Math/278_First_Bad_Version.cpp
@@ -0,0 +1,28 @@
+// Problem: First Bad Version
+// Difficulty: Easy
+// Topic: Math
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=0;
+        int right=n;
+        int ans=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isBadVersion(mid)){
+                ans=mid;
+                right=mid-1;
+            } else {
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+};
