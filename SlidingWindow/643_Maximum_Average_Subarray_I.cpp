9c04a146f3be7c9d7b156d002abd0aae405b4edc
harshhhhss <harshsinghhhh26@gmail.com>
2026-06-03 14:45:18 +0530

    problem 643 maximum average subarray I

---
+++ SlidingWindow/643_Maximum_Average_Subarray_I.cpp
@@ -0,0 +1,24 @@
+// Problem: Maximum Average Subarray I
+// Difficulty: Easy
+// Topic: Sliding Window
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int windowsum=0;
+        for(int i=0;i<k;i++){
+            windowsum+=nums[i];
+        }     
+        int maxsum=windowsum;
+        for(int i=k;i<nums.size();i++){
+            windowsum+=nums[i];
+            windowsum-=nums[i-k];
+            maxsum=max(maxsum,windowsum);
+        }
+        return (double)maxsum/k;
+    }
+};
+
