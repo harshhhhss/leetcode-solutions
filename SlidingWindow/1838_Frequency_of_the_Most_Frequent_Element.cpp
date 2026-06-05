c3ccd4ba5cef41ae6890e4c3e4a655cbcb1a5c8a
harshhhhss <harshsinghhhh26@gmail.com>
2026-06-05 12:20:50 +0530

    problem 1838 frequency of the most frequent element

---
+++ SlidingWindow/1838_Frequency_of_the_Most_Frequent_Element.cpp
@@ -0,0 +1,25 @@
+// Problem: Frequency of the Most Frequent Element
+// Difficulty: Medium
+// Topic: Sliding Window
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        long long sum=0;
+        int i=0;
+        int ans=0;
+        for(int j=0;j<nums.size();j++){
+            sum+=nums[j];
+            while((long long)nums[j] * (j-i+1)-sum>k){
+                sum-=nums[i];
+                i++;
+            }
+            ans=max(ans,j-i+1);
+        }
+        return ans;
+    }
+};
