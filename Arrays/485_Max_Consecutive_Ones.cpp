630ae54469544ed47de4fd980eda20ed55e45438
harshhhhss <harshsinghhhh26@gmail.com>
2026-06-06 22:52:32 +0530

    problem 485 max consecutive ones

---
+++ Arrays/485_Max_Consecutive_Ones.cpp
@@ -0,0 +1,23 @@
+// Problem: Max Consecutive Ones
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int ans=0;
+        for(int x: nums){
+            if(x==1){
+                count++;
+                ans=max(ans,count);
+            } else{
+                count=0;
+            }
+        }
+        return ans;
+    }
+};
