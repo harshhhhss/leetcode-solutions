65f1374200a90542148762a57264a4cdf88c0659
harshhhhss <harshsinghhhh26@gmail.com>
2026-06-09 22:58:04 +0530

    problem 162 find peak element

---
+++ BinarySearch/162_Find_Peak_Element.cpp
@@ -0,0 +1,23 @@
+// Problem: Find Peak Element
+// Difficulty: Medium
+// Topic: Binary Search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+            } else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+};
