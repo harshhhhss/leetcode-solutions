00fe3d9f56e4ac104ea337d8e955596c30647e02
harshhhhss <harshsinghhhh26@gmail.com>
2026-06-14 00:36:48 +0530

    problem 540 single element in a sorted array

---
+++ BinarySearch/540_Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,29 @@
+// Problem: Single Element in a Sorted Array
+// Difficulty: Medium
+// Topic: Binary Search
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l=0;
+        int r=nums.size() - 1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                l=mid+2;
+            } else{
+                r=mid;
+            }
+        }
+        return nums[l];
+    }
+};
+
+
+
