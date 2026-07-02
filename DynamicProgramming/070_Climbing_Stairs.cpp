f0bae1cc0ec8b83f3bbe8f9884168af926362021
harshhhhss <harshsinghhhh26@gmail.com>
2026-07-02 23:08:39 +0530

    problem 70 climbing stairs

---
+++ DynamicProgramming/070_Climbing_Stairs.cpp
@@ -0,0 +1,24 @@
+// Problem: Climbing Stairs
+// Difficulty: Easy
+// Topic: Dynamic Programming
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int prev1=2;
+        int prev2=1;
+        int curr=0;
+        for(int i=2;i<n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return curr;
+    }
+};
