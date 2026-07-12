575e703c2e4b4bbe7df5eceb62b61088d8a28260
harshhhhss <harshsinghhhh26@gmail.com>
2026-07-12 21:51:47 +0530

    problem 392 is subsequence

---
+++ Strings/392_Is_Subsequence.cpp
@@ -0,0 +1,21 @@
+// Problem: Is Subsequence
+// Difficulty: Easy
+// Topic: Strings 
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        int j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return i==s.size();
+    }
+};
