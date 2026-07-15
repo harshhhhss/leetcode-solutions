6b22f1bd86d18164d18f0fa7854da36edb38bcc5
harshhhhss <harshsinghhhh26@gmail.com>
2026-07-15 22:53:07 +0530

    problem 3658 gcd of odd and even sums

---
+++ Math/3658_GCD_of_Odd_and_Even_Sums.cpp
@@ -0,0 +1,13 @@
+// Problem: GCD of Odd and Even Sums
+// Difficulty: Easy
+// Topic: Math
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
