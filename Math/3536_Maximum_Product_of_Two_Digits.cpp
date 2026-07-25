e022e1ca347e66b1ee6500f9d4995e17363d6e0b
harshhhhss <harshsinghhhh26@gmail.com>
2026-07-25 20:36:06 +0530

    problem 3536 maximum product of two digits

---
+++ Math/3536_Maximum_Product_of_Two_Digits.cpp
@@ -0,0 +1,30 @@
+// Problem: Maximum Product of Two Digits
+// Difficulty: Easy
+// Topic: Math
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        int first=n%10;
+        n/=10;
+        int second=n%10;
+        n/=10;
+        if(second>first){
+            swap(first,second);
+        }
+        while(n){
+            int a=n%10;
+            n/=10;
+            if(a>first){
+                second=first;
+                first=a;
+            } else if(a>second){
+                second=a;
+            }
+        }
+        return first*second;
+    }
+};
