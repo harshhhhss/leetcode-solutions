e8a25cebfea43cec0e55d86c25814786a8cbca6a
harshhhhss <harshsinghhhh26@gmail.com>
2026-07-26 22:01:17 +0530

    problem 628 maximum product of three numbers

---
+++ Arrays/628_Maximum_Product_of_Three_Numbers.cpp
@@ -0,0 +1,15 @@
+// Problem: Maximum Product of Three Numbers
+// Difficulty: Easy
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return max(nums[n-1]*nums[n-2]*nums[n-3],nums[0]*nums[1]*nums[n-1]);
+    }
+};
