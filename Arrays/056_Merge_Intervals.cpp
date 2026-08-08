9be4a90a05439043d6a50c811a6fcad0cdfc5fa9
harshhhhss <harshsinghhhh26@gmail.com>
2026-08-08 20:19:14 +0530

    problem 56 merge intervals

---
+++ Arrays/056_Merge_Intervals.cpp
@@ -0,0 +1,21 @@
+// Problem: Merge Intervals
+// Difficulty: Medium
+// Topic: Arrays
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+        }
+        return ans;
+    }
+};
