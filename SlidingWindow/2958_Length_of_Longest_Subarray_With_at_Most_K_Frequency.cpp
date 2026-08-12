257defa58a18598bd7791497e85cccba2cea6a5a
harshhhhss <harshsinghhhh26@gmail.com>
2026-08-12 21:07:49 +0530

    problem 2958 length of longest subarray with at most k frequency

---
+++ SlidingWindow/2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
@@ -0,0 +1,25 @@
+// Problem: Length of Longest Subarray With at Most K Frequency
+// Difficulty: Medium
+// Topic: Sliding Window
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int left=0;
+        int ans=0;
+        for(int right=0;right<nums.size();right++){
+            freq[nums[right]]++;
+            while(freq[nums[right]]>k){
+                freq[nums[left]]--;
+                left++;
+            }
+            ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
+
