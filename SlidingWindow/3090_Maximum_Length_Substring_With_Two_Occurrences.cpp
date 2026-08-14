0fd8a9a07f415aa60f0a576987d1cefffa68ed38
harshhhhss <harshsinghhhh26@gmail.com>
2026-08-14 23:55:16 +0530

    problem 3090 maximum length substring with two occurrences

--- SlidingWindow/3090_Maximum_Length_Substring_With_Two_Occurrences.cpp
+++ SlidingWindow/3090_Maximum_Length_Substring_With_Two_Occurrences.cpp
@@ -5,3 +5,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        unordered_map<char, int> m;
+        int i=0;
+        int j=0;
+        int n=s.size();
+        int res=0;
+        while(j<n){
+            m[s[j]]++;
+            while(m[s[j]]>2){
+                m[s[i]]--;
+                i++;
+            }
+            res=max(res,j-i+1);
+            j++;
+        }
+        return res;
+    }
+};
